1ef38f15ec9643b5fd49cd7e633b8c7ad67443dd
Nicolas Earnshaw <earnshaw.nico@gmail.com>
2017-01-16 17:07:30 -0300

    template emprolijado

--- proyectos/TEMPLATE/TEMPLATE.ino
+++ proyectos/TEMPLATE/TEMPLATE.ino
@@ -4,7 +4,7 @@
 #include <ESP8266mDNS.h>
 
 
-// wifi connection variables
+/////////////////// wifi connection variables
 
 const char* ssid = "NICTOPIA";
 const char* password = "queganasdejoder";
@@ -15,7 +15,9 @@ const int localPort = 7016;
 const char* controllerId = "hab13tel";
 
 
-// specific variables
+/////////////////// END
+
+/////////////////// VARIABLES
 
 const int led = D5;       //normal led w resistance
 const int magnet = D1;    //reed sensor (magnet) on the digital out pin
@@ -23,6 +25,8 @@ const int magnet = D1;    //reed sensor (magnet) on the digital out pin
 int mag = 0;
 boolean magnetState = false;
 
+
+/////////////////// END
 
 
 
@@ -34,9 +38,8 @@ char ReplyBuffer[] = "acknowledged"; // a string to send back
 ESP8266WebServer server(80);   //connecto HTTP in
 
 void setup() {
-  // Initialise Serial connection
+
   Serial.begin(115200);
-  // Initialise wifi connection
   delay(1000);
   wifiConnected = connectWifi();
   // only proceed if wifi connection successful
@@ -49,6 +52,15 @@ void setup() {
     server.on("/reset", handleReset);
 
     server.onNotFound(handleNotFound);
+
+///////////////////     MORE URL OPTIONS
+
+
+//more calls go here
+
+
+/////////////////// END
+
     
     server.begin();
     Serial.println("HTTP server started");
@@ -56,12 +68,12 @@ void setup() {
     if (udpConnected){
 
       
-//////// initialise pins
+///////////////////   initialise pins
 
       pinMode(D5,OUTPUT);
       pinMode(D1,INPUT);
 
-/////
+/////////////////// END
       
     }
   }
@@ -76,7 +88,7 @@ void handleRoot() {
   digitalWrite(LED_BUILTIN, 1);
   delay(500);
 
-/////// DESCRIPTION 
+/////////////////// DESCRIPTION 
 
   String message = "detecta iman\n\n";
   message += controllerId;
@@ -89,7 +101,8 @@ void handleRoot() {
   server.send(200, "text/plain", message);
 
   Serial.println("root request");
-///////
+  
+/////////////////// END
 
   Serial.println("root request");
   digitalWrite(LED_BUILTIN, 0);
@@ -106,48 +119,24 @@ void handleReset()
    server.send(200, "text/plain", "reset");
    Serial.println("reset");
 
-////  VARIABLES TO RESET
+///////////////////  VARIABLES TO RESET
 
   int mag = 0;
   boolean magnetState = false;
 
-////
+/////////////////// END
    
 }
 
 
 
-//////  HANDLE OTHER CALLS
+///////////////////  HANDLE OTHER CALLS
 
 
-/////
+// other call functions go here
 
 
-
-
-void handleNotFound(){
-  digitalWrite(LED_BUILTIN, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(LED_BUILTIN, 0);
-}
-
-
-
-
-
-
-
+/////////////////// END
 
 
 
@@ -165,7 +154,7 @@ if(wifiConnected){
       delay(15);
 
 
-  ///////////// SPECIFIC CODE
+/////////////////// ACTUAL CODE
   
       mag = digitalRead(magnet);
      // Serial.println(mag);
@@ -190,102 +179,10 @@ if(wifiConnected){
     }
 
 
-  //////////
+/////////////////// END
     
     }
   }
 }
 
 
-// read UDP incoming packages
-
-void UDPRead()
-{
-     // if there’s data available, read a packet
-      
-      int packetSize = UDP.parsePacket();
-      if(packetSize)
-      {
-        Serial.println("");
-        Serial.print("Received packet of size ");
-        Serial.println(packetSize);
-        Serial.print("From ");
-        IPAddress remote = UDP.remoteIP();
-        for (int i =0; i < 4; i++)
-        {
-          Serial.print(remote[i], DEC);
-          if (i < 3)
-          {
-            Serial.print(".");
-          }
-        }
-        Serial.print(", port ");
-        Serial.println(UDP.remotePort());
-        
-        // read the packet into packetBufffer
-        UDP.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
-        Serial.println("Contents:");
-        int value = packetBuffer[0]*256 + packetBuffer[1];
-        Serial.println(value);
-        
-        Serial.print("Packet contents: [");
-        Serial.print(packetBuffer[0], packetSize);
-        Serial.println("]");
-        
-        // turn LED on or off depending on value recieved
-        digitalWrite(D5,HIGH);
-        delay(1000);
-        digitalWrite(D5,LOW);        
-      }
-}
-
-
-// connect to UDP – returns true if successful or false if not
-boolean connectUDP(){
-  boolean state = false;
-  
-  Serial.println("");
-  Serial.println("Connecting to UDP”");
-  
-  if(UDP.begin(localPort) == 1){
-    Serial.println("Connection successful");
-    state = true;
-  }
-  else{
-    Serial.println("Connection failed");
-  }
-  
-  return state;
-  }
-// connect to wifi – returns true if successful or false if not
-boolean connectWifi(){
-  boolean state = true;
-  int i = 0;
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.println("Connecting to WiFi");
-  
-  // Wait for connection
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    if (i > 10){
-      state = false;
-      break;
-    }
-    i++;
-  }
-  if (state){
-    Serial.println("");
-    Serial.print("Connected to ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-  else {
-    Serial.println("");
-    Serial.println("Connection failed.");
-  }
-  return state;
-}
